8daf8def187c64c0e7ceb51b819196d6538ff01e
Ben Grant <benjamin.j.grant@gmail.com>
2021-09-07 09:43:22 -0700

    Update name and profile pic in database when a user logs in

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,10 @@ class User < ApplicationRecord
     devise :rememberable, :omniauthable, omniauth_providers: [:discord]
 
     def self.from_omniauth(auth)
-        where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-            user.name = "#{auth.info.name}:#{auth.info.discriminator}"
-            user.image = auth.info.image
-        end
+        user = where(provider: auth.provider, uid: auth.uid).first_or_create
+        user.name = "#{auth.info.name}##{auth.extra.raw_info.discriminator}"
+        user.image = auth.info.image
+        user.save
+        return user
     end
 end
