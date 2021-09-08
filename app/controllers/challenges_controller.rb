c37f3155f94f40eabac5fa848528c35ab1c2a14e
Ben Grant <benjamin.j.grant@gmail.com>
2021-09-07 21:46:14 -0700

    New challenge boilerplate

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,9 +1,15 @@
 class ChallengesController < ApplicationController
+    before_action :authenticate_user!, except: [:index, :show]
+
     def index
         @challenges = Challenge.all
     end
 
     def show
         @challenge = Challenge.find(params[:id])
+    end
+
+    def new
+        @challenge = Challenge.new
     end
 end
