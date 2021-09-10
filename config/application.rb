acb5212681a52534e66ce51ad082ca4940d41621
Ben Grant <benjamin.j.grant@gmail.com>
2021-09-09 22:20:56 -0700

    I changed my mind

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Av1nator
+module Tallynator
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
